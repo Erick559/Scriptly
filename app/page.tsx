e605c0a70acff8c32427e7f60cfef509f1e66891
Erick559 <erickungu82@gmail.com>
2024-10-01 14:56:29 +0300

    feat:Import the navigation bar in the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
+import React from 'react';
 import LoginButton from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { Manrope } from "next/font/google";
 import { SocialIcon } from 'react-social-icons'
+import NavigationBar from "@/components/navbar";
 
 const manrope = Manrope({weight:['300','400','700'],subsets:['latin']});
 
 export default function Home() {
   return (
-    <div className="mt-[100px]">
-      <div className="h-screen">
-          Hero
+    <>
+      <NavigationBar />
+      <div className="mt-[100px]">
+        <div className="h-screen">
+            Hero
+        </div>
       </div>
-    </div>
+    </>
   );
 }
