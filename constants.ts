d778f449849b75dd211b3c78eddc95fd34a8ed45
Erick559 <erickungu82@gmail.com>
2024-10-01 14:09:08 +0300

    feat: Add a constants file holding constant variables.

---
+++ constants.ts
@@ -0,0 +1,19 @@
+const navLinks = [
+    {
+        id: 1,
+        name: 'About',
+        href:'#about',
+    },
+    {
+        id:2,
+        name: 'Features',
+        href: '#features',
+    },
+    {
+        id:3,
+        name:'Pricing',
+        href:'#pricing',
+    },
+]
+
+export default navLinks;
