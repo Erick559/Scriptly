473fbcbdb586b5550620b16f7d7d5c77579362e8
Erick559 <erickungu82@gmail.com>
2024-10-03 16:19:40 +0300

    fix: Decrease the padding size of the content section of the hero.

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -8,12 +8,12 @@ const Hero = () => {
       <div className="h-full w-full dark:bg-black bg-blue-600 dark:bg-grid-white/[0.2] bg-grid-white/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-blue-600 [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black_70%)]"></div>
-        <div className='flex flex-col items-center justify-center z-20 gap-10 p-5'>
+        <div className='flex flex-col items-center justify-center z-20 gap-10 px-2'>
           <div className = 'bg-blue-400 bg-opacity-70 backdrop-blur-lg rounded-full py-2 px-10'>
             <p className='text-white text-sm font-medium'>✨ Writing made easy</p>
           </div>
           <div className='flex flex-col items-center justify-center gap-3'>
-            <h1 className='text-white text-4xl sm:text-7xl font-extrabold text-center'>
+            <h1 className='text-white text-3xl sm:text-7xl font-extrabold text-center'>
               Write your documents easily with Scriptly. 
             </h1>
             <p className='text-stone-300 text-md text-center w-[30ch]'>Scriptly has AI capabilities that allow you to write your documents fast and easily. Try it out now, It&apos;s free!</p>
