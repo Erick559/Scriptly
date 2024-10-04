d3a28be720b69cf93a65485f08ceae0cc753427d
Erick559 <erickungu82@gmail.com>
2024-10-04 09:51:30 +0300

    fix(hero): Add top padding for smaller devices as it impeds the navgation bar space.

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
     }
   };
   return (
-    <section className='h-screen w-full flex flex-col justify-center items-center bg-blue-600'>
+    <section className='h-screen w-full flex flex-col justify-center items-center bg-blue-600 pt-24'>
       <div className="h-full w-full dark:bg-black bg-blue-600 dark:bg-grid-white/[0.2] bg-grid-white/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-blue-600 [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black_60%)]"></div>
