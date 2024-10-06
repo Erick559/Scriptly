2cf19b9f9710458447134ad8c7dbce6c021de107
Erick559 <erickungu82@gmail.com>
2024-10-06 17:51:14 +0300

    Implement dynamic imports for CallToAction and Footer components
    - Added dynamic imports for CallToAction and Footer components
    - Used Next.js dynamic function to enable code splitting
    - Disabled server-side rendering for these components
    - Improved initial page load performance by reducing bundle size

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import dynamic from 'next/dynamic';
 import { Manrope } from "next/font/google";
 import NavigationBar from "@/components/navbar";
 import Hero from './components/hero';
-import Footer from '@/app/components/footer';
-import CallToAction from './components/call-to-action';
 
 const Features = dynamic(() => import('./components/features'), { ssr: false });
+const CallToAction = dynamic(() => import('./components/call-to-action'), { ssr: false });
+const Footer = dynamic(() => import('./components/footer'), { ssr: false });
 
 const manrope = Manrope({weight:['300','400','700'],subsets:['latin']});
 
