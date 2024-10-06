3ce96a8d0ac22ea56c34d319be0a44f8a04e6d80
Erick559 <erickungu82@gmail.com>
2024-10-06 17:21:24 +0300

    fix: Render white logo well.

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className='bg-blue-500 py-6 md:py-4'>
       <div className='flex flex-col justify-between items-center gap-5'>
         <div>
-          <Image src='/Logo-white.svg' alt="Scriplty Logo" width={100} height={100} className='md:w-[150px] md:h-[150px]' />
+          <Image src='/Logo-White.svg' alt="Scriplty Logo" width={100} height={100} className='md:w-[150px] md:h-[150px]' />
         </div>
         <div>
           <ul className='flex gap-7 w-full'>
